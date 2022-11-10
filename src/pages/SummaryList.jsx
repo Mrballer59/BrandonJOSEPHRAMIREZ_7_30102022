3cf18a76df1486ec0ecfb470a9d93a14729ceee1
B <77552838+Mrballer59@users.noreply.github.com>
2022-11-10 20:15:28 +0000

    second page added +css table added neex to check with Nissim 💻

--- src/pages/SummaryList.jsx
+++ src/pages/SummaryList.jsx
@@ -1,6 +1,9 @@
 import React, { useContext } from "react";
 import Table from "../components/tableSummary/Table";
 import { DataContext } from "../data/DataContext";
+import "./SummaryList.css";
+import { sortType } from "../config/StateSelector";
+// import StateSeletor from "../components/stateSelector/StateSeletor";
 
 const SummaryList = () => {
   const data = useContext(DataContext);
@@ -9,8 +12,15 @@ const SummaryList = () => {
     <div className="container">
       <h1>Current Emplyees</h1>
       <div>
-        <div>
+        <div className="entries-selector">
           <span>Show</span>
+          <select>
+            <option>10</option>
+            <option>15</option>
+            <option>20</option>
+            <option>25</option>
+            <option>30</option>
+          </select>
           <span>entries</span>
         </div>
       </div>
