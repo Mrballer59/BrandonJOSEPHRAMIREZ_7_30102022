b63f01dcc9957460c7a4ebef22744860dc6dfd6d
B <77552838+Mrballer59@users.noreply.github.com>
2022-11-14 17:50:40 +0000

    sort problem sorted

--- src/components/tableSummary/Table.jsx
+++ src/components/tableSummary/Table.jsx
@@ -7,27 +7,22 @@ import { DataContext } from "../../data/DataContext";
 
 const Table = () => {
   const userData = useContext(DataContext);
-  const [data, setData] = useState(MockData);
+  //sort colmns
+  const [data, setData] = useState();
   const [order, setOrder] = useState("ASC");
 
-  console.log(userData.UserInfo);
+  // console.log(userData.UserInfo);
 
   const sorting = (col) => {
     if (order === "ASC") {
-      const sorted = [...data].sort((a, b) => {
-        console.log(a);
-
-        console.log(b);
-
-        console.log(col);
-
+      const sorted = userData.UserInfo.sort((a, b) => {
         return a[col].toLowerCase() > b[col].toLowerCase() ? 1 : -1;
       });
       setData(sorted);
       setOrder("DSC");
     }
     if (order === "DSC") {
-      const sorted = [...data].sort((a, b) =>
+      const sorted = userData.UserInfo.sort((a, b) =>
         a[col].toLowerCase() < b[col].toLowerCase() ? 1 : -1
       );
       setData(sorted);
@@ -37,18 +32,26 @@ const Table = () => {
 
   return (
     <div>
+      <div>
+        <input
+          type="text"
+          placeholder="search"
+          // value={searchPharse}
+          // onChange={search}
+        />
+      </div>
       <table>
         <tbody>
           <tr className="col-title">
             <th onClick={() => sorting("firstName")}>First Name</th>
             <th onClick={() => sorting("lastName")}>Last Name</th>
-            <th>Start Date</th>
-            <th>Departement </th>
-            <th>Date of Birth</th>
-            <th>Street</th>
-            <th>City </th>
-            <th>state </th>
-            <th>Zip code </th>
+            <th onClick={() => sorting("startDate")}>Start Date</th>
+            <th onClick={() => sorting("departement")}>Departement </th>
+            <th onClick={() => sorting("dateOfBirth")}>Date of Birth</th>
+            <th onClick={() => sorting("street")}>Street</th>
+            <th onClick={() => sorting("city")}>City </th>
+            <th onClick={() => sorting("state")}>state </th>
+            <th onClick={() => sorting("zipCode")}>Zip code </th>
           </tr>
 
           {userData.UserInfo.map((user, index) => (
@@ -71,48 +74,3 @@ const Table = () => {
 };
 
 export default Table;
-
-// const sorting = (col) => {
-//   if (order === "ASC") {
-//     const sorted = [...data].sort((a, b) => {
-//       console.log(a);
-//       return a[col].toLowerCase() > b[col].toLowerCase() ? 1 : -1;
-//     });
-
-//     setData(sorted);
-//     setOrder("DSC");
-//   }
-//   if (order === "DSC") {
-//     const sorted = [...data].sort((a, b) =>
-//       a[col].toLowerCase() < b[col].toLowerCase() ? 1 : -1
-//     );
-//     setData(sorted);
-//     setOrder("ASC");
-//   }
-// };
-
-{
-  /* <th onClick={() => sorting("first_name")}>First Name</th>
-            <th onClick={() => sorting("last_name")}>Last Name</th>
-            <th onClick={() => sorting("start_date")}>Start Date</th>
-            <th onClick={() => sorting("departement")}>Departement </th>
-            <th onClick={() => sorting("date_of_birth")}>Date of Birth</th>
-            <th onClick={() => sorting("street")}>Street</th>
-            <th onClick={() => sorting("city")}>City </th>
-            <th onClick={() => sorting("first_name")}>state </th>
-            <th onClick={() => sorting("first_name")}>Zip code </th> */
-}
-
-{
-  /* {UserInfo.map((e,) => (
-            <tr key={e.id}>
-              <td>{e.first_name}</td>
-              <td>{e.last_name}</td>
-              <td>{e.start_date}</td>
-              <td>{e.departement}</td>
-              <td>{e.date_of_birth}</td>
-              <td>{e.street}</td>
-              <td>{e.city}</td>
-            </tr>
-          ))} */
-}
